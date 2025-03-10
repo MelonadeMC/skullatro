8348d28b6a1bd0852bc15e93e368c0ace098f4b9
MelonadeMC <melonadeprevmc@gmail.com>
2025-03-10 01:00:06 -0400

    added a couple of new jokers

--- items/jokers.lua
+++ items/jokers.lua
@@ -171,3 +171,176 @@ SMODS.Joker{
         end
     end
 }
+
+-- TODO: FINISH cuz i dont know what im doing so im putting this off
+SMODS.Joker{
+    name = "Parasoul",
+    key = "parasoul",
+    loc_txt = {
+        name = "Parasoul",
+        text = {
+            "All {C:attention}played cards{} are",
+            "considered {C:spades}Spades{} when",
+            "played hand contains",
+            "a {C:attention}Queen of Spades{}"
+        }
+    },
+    rarity = 3,
+    cost = 8,
+    blueprint_compat = false,
+    atlas = "skullatroJokers",
+    pos = {x = 9, y = 0},
+    config = {
+        extra = {
+            foundQoS = false
+        }
+    },
+    loc_vars = function(self,info_queue,card)
+        return { vars = {
+            card.ability.extra.foundQoS
+        } }
+    end,
+    calculate = function(self,card,context)
+        for i,v in ipairs(G.play.cards) do
+            if v:get_id() == 12 and v:is_suit('Spades') then
+                print("ok cool")
+                card.ability.extra.foundQoS = true
+                return
+            end
+        end
+
+        if context.after and context.cardarea == G.play or context.end_of_round then
+            card.ability.extra.foundQoS = false
+        end
+    end
+}
+
+SMODS.Joker{
+    name = "Ms. Fortune",
+    key = "msfortune",
+    loc_txt = {
+        name = "Ms. Fortune",
+        text = {
+            "When a card is sold,",
+            "add the {C:attention}sell value{} of that card",
+            "to this Joker's sell value"
+        }
+    },
+    rarity = 1,
+    cost = 5,
+    blueprint_compat = true,
+    atlas = "skullatroJokers",
+    pos = {x = 3, y = 0},
+    calculate = function(self,card,context)
+        if context.card_sold then
+            --print("sold card received in context")
+            card.sell_cost = card.sell_cost + context.card_being_sold.sell_cost
+
+            return {
+                message = localize('k_val_up'),
+                colour = G.C.MONEY
+            }
+        end
+    end
+}
+
+SMODS.Joker{
+    name = "Painwheel",
+    key = "painwheel",
+    loc_txt = {
+        name = "Painwheel",
+        text = {
+            "This Joker gains {X:mult,C:white}X#1#{} Mult",
+            "when a Wheel of Fortune fails",
+            "during this ante.",
+            "Currently {X:mult,C:white}X#2#{} Mult"
+        }
+    },
+    rarity = 2,
+    cost = 5,
+    blueprint_compat = true,
+    atlas = "skullatroJokers",
+    pos = {x = 5, y = 0},
+    config = {
+        extra = {
+            gains = 0.5,
+            currentxMult = 1
+        }
+    },
+    loc_vars = function(self,info_queue,card)
+        return { vars = {
+            card.ability.extra.gains,
+            card.ability.extra.currentxMult
+        } }
+    end,
+    calculate = function(self, card, context) -- i did not just steal wheel of hope code from cryptid what are you talking about
+		if context.joker_main and (card.ability.extra.currentxMult > 1) then
+			return {
+				message = localize({ type = "variable", key = "a_xmult", vars = { card.ability.extra.currentxMult } }),
+				Xmult_mod = card.ability.extra.currentxMult,
+			}
+		end
+		if context.consumeable then
+			if context.consumeable.ability.name == "The Wheel of Fortune" and not context.consumeable.cry_wheel_success then
+				card.ability.extra.currentxMult = card.ability.extra.currentxMult + card.ability.extra.gains
+				card_eval_status_text(
+					card,
+					"extra",
+					nil,
+					nil,
+					nil,
+					{ message = localize({ type = "variable", key = "a_xmult", vars = { card.ability.extra.currentxMult } }) }
+				)
+				return nil, true
+			end
+		end
+        if context.end_of_round and context.cardarea == G.jokers and G.GAME.blind.boss then
+            card.ability.extra.currentxMult = 1
+
+            return {
+                message = localize('k_reset'),
+                colour = G.C.RED
+            }
+        end
+	end,
+}
+
+SMODS.Joker{
+    name = "Valentine",
+    key = "valentine",
+    loc_txt = {
+        name = "Valentine",
+        text = {
+            "{C:mult}+#1#{} Mult and {C:chips}+#2#{} Chips",
+            "for each scored card",
+            "with the {C:hearts}Hearts{} suit."
+        }
+    },
+    rarity = 2,
+    cost = 5,
+    blueprint_compat = true,
+    atlas = "skullatroJokers",
+    pos = {x = 8, y = 0},
+    config = {
+        extra = {
+            mult = 2,
+            chips = 14
+        }
+    },
+    loc_vars = function(self,info_queue,card)
+        return { vars = {
+            card.ability.extra.mult,
+            card.ability.extra.chips
+        } }
+    end,
+    calculate = function(self,card,context)
+        if context.individual and context.cardarea == G.play then
+            if context.other_card:is_suit('Hearts') then
+                return {
+                    mult = card.ability.extra.mult,
+                    chips = card.ability.extra.chips
+                }
+            end
+        end
+    end
+}
--- misc/hooks.lua
+++ misc/hooks.lua
@@ -13,3 +13,24 @@ function Card:remove()
     end
     card_remove_ref(self)
 end
+
+-- wtf is going wrong im leaving this for later
+-- local card_is_suit_ref = Card.is_suit
+-- function Card:is_suit(suit, bypass_debuff, flush_calc)
+--     for i = 1, #G.jokers.cards do
+--         if G.jokers.cards[i].label == "Parasoul" then
+--             if G.jokers.cards[i].ability.extra.foundQoS == true then
+--                 return true
+--             end
+--         end
+--     end
+
+--     self:card_is_suit_ref(suit, bypass_debuff, flush_calc)
+-- end
+
+local sell_card_ref = Card.sell_card
+function Card:sell_card()
+    SMODS.calculate_context({card_sold = true , card_being_sold = self})
+    --print(self.label .. " sold")
+    sell_card_ref(self)
+end
